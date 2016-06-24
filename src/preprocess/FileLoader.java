da69bfc9b22d100dd7a2bb85ca373d2a9d050e9e
neil-justice <neil-justice@users.noreply.github.com>
2016-06-24 12:45:36 +0100

    refactored filemanager

---
+++ src/preprocess/FileLoader.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class FileLoader {
+  
+  public static void loadList(String filename, Collection<String> coll) {
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+      String line;
+      
+      while ((line = reader.readLine()) != null) {
+        coll.add(line.toLowerCase());
+      }
+    } catch(Exception e) {
+      throw new Error("file not found");
+    }
+  }
+  
+  public static void processFile(String in, String out, LineOperator op) {
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(new File(in)));
+      BufferedWriter writer = new BufferedWriter(new FileWriter(new File(out)));
+      String line;
+      int cnt = 0;
+      
+      while ((line = reader.readLine()) != null) {
+        cnt++;
+        String outString = op.operate(line);
+        writer.write(outString);
+        writer.newLine();
+        if (cnt % 100 == 0) writer.flush();
+      }
+      writer.flush();
+    } catch (FileNotFoundException e) {
+      throw new Error("input file not found at " + in);
+    } catch (IOException e) {
+      throw new Error("IO error");
+    }      
+  }
+  
+  public interface LineOperator {
+    public String operate(String in);
+  }
+}
